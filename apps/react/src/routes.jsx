4eedbb3f10fa333efc77c56052dd7190065206e1
DrAmanJy <dramanjy0770@gmail.com>
2026-02-20 22:25:54 +0530

    added lucide-react

--- apps/react/src/routes.jsx
+++ apps/react/src/routes.jsx
@@ -1,5 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Settings from "./pages/Settings";
+import Layout from "./components/Layout";
 
-const router = createBrowserRouter([{ index: true, element: <Home /> }]);
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "projects", element: <Projects /> },
+      { path: "settings", element: <Settings /> },
+    ],
+  },
+]);
 export default router;
