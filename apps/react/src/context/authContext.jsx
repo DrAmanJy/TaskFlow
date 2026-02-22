5cb71b02b53ed60b9ca54fc43fe48eae890e24c2
DrAmanJy <dramanjy0770@gmail.com>
2026-02-22 13:51:30 +0530

    added register function

--- apps/react/src/context/authContext.jsx
+++ apps/react/src/context/authContext.jsx
@@ -2,8 +2,22 @@ import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext(null);
 export default function AuthProvider({ children }) {
-  const [user, setUser] = useState({ isLogin: false });
+  const [user, setUser] = useState(undefined);
+  const register = async (data) => {
+    const res = await fetch("http://localhost:3000/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const user = await res.json();
+    setUser(user);
+    return true;
+  };
 
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, register }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 export const useAuth = () => useContext(AuthContext);
