1ff6479c48d6ff92c1917c57e5346275601287d0
DrAmanJy <dramanjy0770@gmail.com>
2026-02-23 16:21:45 +0530

    add Toaster component for notifications

--- apps/react/src/App.jsx
+++ apps/react/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import AuthProvider from "./context/authContext";
@@ -6,6 +6,7 @@ import AuthProvider from "./context/authContext";
 export default function App() {
   return (
     <AuthProvider>
+      <Toaster position="top-right" />
       <RouterProvider router={router}></RouterProvider>
     </AuthProvider>
   );
