8d67c684440e65e5aae1e8c0a9934ca84abc52de
DrAmanJy <dramanjy0770@gmail.com>
2026-02-23 22:19:16 +0530

    refactor server setup for improved environment variable handling and middleware configuration

--- apps/express/src/server.js
+++ apps/express/src/server.js
@@ -1,18 +1,27 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { configDotenv } from "dotenv";
+import cookieParser from "cookie-parser";
+
 import connectDb from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
-configDotenv();
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT;
-const mongoUri = process.env.mongoUri;
+const PORT = process.env.PORT || 5000;
+const mongoUri = process.env.MONGO_URI;
+
+// Connect to Database
 connectDb(mongoUri);
 
-app.use(cors({ credentials: true }));
+// Middleware
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", authRouter);
+app.use("/api", projectRouter);
 
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`),
