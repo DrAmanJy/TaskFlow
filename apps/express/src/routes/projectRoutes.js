33fdf512e3bb01ef2365df36e96ba366bddc6385
DrAmanJy <dramanjy0770@gmail.com>
2026-02-23 22:19:01 +0530

    add project routes for CRUD operations with authentication

---
+++ apps/express/src/routes/projectRoutes.js
@@ -0,0 +1,17 @@
+import e from "express";
+import {
+  createProject,
+  deleteProject,
+  readProject,
+  updateProject,
+} from "../controllers/projectController.js";
+import { requireAuth } from "../middlewares/authMiddleware.js";
+
+const Router = e.Router();
+
+Router.get("/project/:id", requireAuth, readProject);
+Router.post("/project", requireAuth, createProject);
+Router.put("/project", requireAuth, updateProject);
+Router.delete("/project/:id", requireAuth, deleteProject);
+
+export default Router;
