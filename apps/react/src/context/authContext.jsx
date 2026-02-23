b129a344a2c34ba5c79797aebc016023ed43d4a9
DrAmanJy <dramanjy0770@gmail.com>
2026-02-23 16:23:08 +0530

    enhance AuthProvider to manage user login state and improve error handling

--- apps/react/src/context/authContext.jsx
+++ apps/react/src/context/authContext.jsx
@@ -1,9 +1,47 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext(null);
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(undefined);
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    let isMounted = true;
 
+    const getUser = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/auth/me", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (!res.ok) {
+          if (isMounted) {
+            setUser(null);
+            setIsLogin(false);
+          }
+          return;
+        }
+
+        const data = await res.json();
+
+        if (isMounted) {
+          setUser(data.user);
+          setIsLogin(true);
+        }
+      } catch (error) {
+        if (isMounted) {
+          setUser(null);
+          setIsLogin(false);
+        }
+      }
+    };
+
+    getUser();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
   const register = async (userInfo) => {
     try {
       const res = await fetch("http://localhost:3000/api/auth/register", {
@@ -23,6 +61,7 @@ export default function AuthProvider({ children }) {
       }
 
       setUser(data.user);
+      setIsLogin(true);
       return { success: true, message: data.message };
     } catch (e) {
       return { success: false, message: "Network error, please try again" };
@@ -48,6 +87,7 @@ export default function AuthProvider({ children }) {
       }
 
       setUser(data.user);
+      setIsLogin(true);
       return { success: true, message: data.message };
     } catch (e) {
       return { success: false, message: "Network error, please try again" };
@@ -55,7 +95,7 @@ export default function AuthProvider({ children }) {
   };
 
   return (
-    <AuthContext.Provider value={{ user, register, login }}>
+    <AuthContext.Provider value={{ user, isLogin, register, login }}>
       {children}
     </AuthContext.Provider>
   );
