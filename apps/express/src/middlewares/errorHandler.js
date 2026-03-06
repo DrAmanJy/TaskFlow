dd62e897ae72953ab11c05fdca1b1ac9ee2abc96
DrAmanJy <dramanjy0770@gmail.com>
2026-03-06 13:43:56 +0530

    refactor: implement global response utility and enhanced error middleware

--- apps/express/src/middlewares/errorHandler.js
+++ apps/express/src/middlewares/errorHandler.js
@@ -1,22 +1,52 @@
+import AppError from "../utils/AppError.js";
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR: ", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
 export default function errorHandler(err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  let error = { ...err, message: err.message, name: err.name };
-
-  if (error.isOperational) {
-    res.status(error.statusCode).json({
-      success: false,
-      message: error.message,
-    });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
   } else {
-    console.error("ERROR", error);
-    res.status(500).json({
-      success: false,
-      message:
-        process.env.NODE_ENV === "development"
-          ? error.message
-          : "Something went very wrong!",
-    });
+    let error = { ...err, message: err.message };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+    sendErrorProd(error, res);
   }
 }
